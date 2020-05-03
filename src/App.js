ffee3e22641724d0cf90ca8a52a1829edd45aec0
mohan <mohan.krishna4220@gmail.com>
2020-05-03 13:55:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,47 +2,16 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Layout";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./helpers/Routes";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <Header />
+      {/* <Home/> */}
       <Router>
-        <Header />
         <Routes />
-        {/* <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        </div> */}
-        {/* <Switch>
-          <Route Link ={}></Route>
-        </Switch> */}
       </Router>
     </div>
   );
--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -1,5 +1,5 @@
 import React,{Component } from 'react';
-import {Router, Switch, Route} from 'react-router-dom';
+import {Router, Switch, Route, Redirect} from 'react-router-dom';
 import Home from '../Home';
 import history from './History';
 import BookingDetails from '../Components/Rental/BookingDetails';
@@ -12,9 +12,7 @@ export default class Routes extends Component{
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/booking-details" component={BookingDetails} />
-                    {/* <Route path="/About" component={About} />
-                    <Route path="/Contact" component={Contact} />
-                    <Route path="/Products" component={Products} /> */}
+                    <Redirect handler={Home}/>
                 </Switch>
             </Router>
         )
