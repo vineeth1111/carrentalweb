fed98683056d61ce815df89758fead4663b4a955
mohan <mohan.krishna4220@gmail.com>
2020-05-03 13:33:30 +0530

    Updates

---
+++ precache-manifest.44b43008a0da501197e4cbaf72aef5c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583e798f2e33fe76ed6a33fc2fdbd7ba",
+    "url": "/carrentalweb/index.html"
+  },
+  {
+    "revision": "ea99231b706a8cc2a05c",
+    "url": "/carrentalweb/static/css/main.164a379e.chunk.css"
+  },
+  {
+    "revision": "7937f0fe6eb460342f56",
+    "url": "/carrentalweb/static/js/2.8afea409.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/carrentalweb/static/js/2.8afea409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea99231b706a8cc2a05c",
+    "url": "/carrentalweb/static/js/main.6432e891.chunk.js"
+  },
+  {
+    "revision": "0036e95f5cd16d1744a7",
+    "url": "/carrentalweb/static/js/runtime-main.6b87fefe.js"
+  },
+  {
+    "revision": "c841ecca4b4c626bb16938e9bfe5e45d",
+    "url": "/carrentalweb/static/media/carLogo.c841ecca.png"
+  },
+  {
+    "revision": "e6a73dd53af91e3a9c7e119eeadf23dc",
+    "url": "/carrentalweb/static/media/hondaAmaze.e6a73dd5.jpg"
+  },
+  {
+    "revision": "bcb8b97942412c2205906a559ed55be0",
+    "url": "/carrentalweb/static/media/hondaCRV.bcb8b979.jpg"
+  },
+  {
+    "revision": "bcb8e516305d21da6f148a9bad98eff8",
+    "url": "/carrentalweb/static/media/hyundaiVenue.bcb8e516.jpg"
+  },
+  {
+    "revision": "9184990fd4768229765fcfb06dd2dfba",
+    "url": "/carrentalweb/static/media/vwPolo.9184990f.jpg"
+  }
+]);
